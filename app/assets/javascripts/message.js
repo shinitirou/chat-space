1fe241d5ef941271b54ad781a49062ca42af49e1
山守真一朗 <shinichiro0917@icloud.com>
2019-07-17 11:58:15 +0900

    add to done method and HTML
    doneメソッドとTHMLの追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,22 @@
 
 $(function(){
+
+  function buildHTML(comment){
+    var html = `<div class='message'>
+                   <div class='message-username'>
+                   ${message.message-username}
+                   </div>
+                   <div class='message-date'>
+                   ${message.message-date}
+                   </div>
+                   <div class='message-text'>
+                   ${message.message-text}
+                   </div>
+                </div>`
+    return html;
+  }
+
+  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +28,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.right__main').append(html)
+      $('.content').val('')
     })
   })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
-json.text  @message.text
+json.content  @message.content
 json.user_id  @message.user.id
 json.image  @message.image
+json.date  @message.created_at
