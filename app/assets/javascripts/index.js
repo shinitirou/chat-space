f2505c686bfba5e0994f79b9c4de906bb496a2bc
山守真一朗 <shinichiro0917@icloud.com>
2019-07-21 13:27:56 +0900

    add json html
    search.js⇨index.jsに名前変更
    jsonで処理された場合のhtmlを追加

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,31 @@
+$(function() {
+
+
+  function appendUser(user){
+    var html =      
+             ` <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>`
+            return html;
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+  });
+  $.ajax({
+    type: 'GET',
+    url: '/users',
+    data: { keyword: input },
+    dataType: 'json'
+  })
+  
+  .done(function(users) {
+    $("#user-search-result").empty();
+    if (users.length !== 0) {
+      users.forEach(function(user){
+        appendUser(user);
+      });
+    }
+  })
+});
