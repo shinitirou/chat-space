9112b0d7b1624304ff4c7a3efd3552ef43e66998
山守真一朗 <shinichiro0917@icloud.com>
2019-07-26 13:05:33 +0900

    create index
    取得したIDよりも後のメッセージを取得する

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @nmesages = @group.messages.where(" id > ?", params[:id])
   end
 end
